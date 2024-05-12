30b5793d118da384d993b0df7454414b95eb0720
mrwhoknows55 <avdhuttanugade55@gmail.com>
2024-05-12 22:39:29 +0530

    feat: added start and stop server button on android screen

--- composeApp/src/androidMain/kotlin/com/mrwhoknows/findmynoti/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/mrwhoknows/findmynoti/MainActivity.kt
@@ -6,13 +6,24 @@ import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
-import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.app.NotificationManagerCompat
 import com.mrwhoknows.findmynoti.server.NotificationServer
+import com.mrwhoknows.findmynoti.ui.theme.AppTheme
 import org.koin.android.ext.android.inject
 
 class MainActivity : ComponentActivity() {
@@ -25,12 +36,36 @@ class MainActivity : ComponentActivity() {
         // todo: add ui to this
         if (!NotificationManagerCompat.getEnabledListenerPackages(this).contains(packageName)) {
             openNotificationAccessSettings()
-        } else {
-            server.startServer()
         }
+
         setContent {
-            Column {
-                App()
+            AppTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    var serverStarted by remember { mutableStateOf(false) }
+                    val text by remember(serverStarted) {
+                        mutableStateOf(
+                            if (serverStarted) "Stop Server" else "Start Server"
+                        )
+                    }
+                    Column(
+                        Modifier.fillMaxWidth(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Button(
+                            onClick = {
+                                serverStarted = !serverStarted
+                                if (serverStarted) {
+                                    server.startServer()
+                                } else {
+                                    server.stopServer()
+                                }
+                            },
+                        ) {
+                            Text(text)
+                        }
+                    }
+                }
             }
         }
     }
